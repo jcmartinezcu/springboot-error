d7c29b99143451c2dfe18c7056aceba872e6b10e
jcmartinez <jcmartinezcu@gmail.com>
2024-07-02 18:35:35 +0200

    personalizando error 404

--- src/main/java/com/jc/curso/springboot/error/springbooterror/controller/HandlerExceptionController.java
+++ src/main/java/com/jc/curso/springboot/error/springbooterror/controller/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.jc.curso.springboot.error.springbooterror.models.Error;
 
@@ -23,6 +24,18 @@ public class HandlerExceptionController {
 
        // return ResponseEntity.internalServerError().body(error);
        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
+
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api rest no encontrada!");
+        error.setMessage(e.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
 
 }

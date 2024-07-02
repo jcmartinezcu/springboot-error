4b51def9b7f1f58fb90442f7faaa39dfc48c4c7e
jcmartinez <jcmartinezcu@gmail.com>
2024-07-02 18:48:20 +0200

    personalizando error numberException

--- src/main/java/com/jc/curso/springboot/error/springbooterror/controller/HandlerExceptionController.java
+++ src/main/java/com/jc/curso/springboot/error/springbooterror/controller/HandlerExceptionController.java
@@ -1,10 +1,13 @@
 package com.jc.curso.springboot.error.springbooterror.controller;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -26,6 +29,19 @@ public class HandlerExceptionController {
        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
     }
 
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception ex){
+        
+        Map<String,Object> error = new HashMap<>();
+        error.put("date", new Date().toString());
+        error.put("error", "numero invalido o incorrecto, no tiene formato de digito!");
+        error.put("message", ex.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return error;
+    }
+
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<Error> notFoundEx(NoHandlerFoundException e){
 
@@ -37,5 +53,7 @@ public class HandlerExceptionController {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
+
+
 
 }
